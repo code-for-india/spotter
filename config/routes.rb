cced23dcdbb1ace6b22e2ade5c1852f731693a42
AnuragRamdasan <anuragr@live.com>
2014-01-04 21:10:52 +0530

    Custom route for show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Spotter::Application.routes.draw do
   resources :issues
-
+  get 'issues/:id/:lat/:lng/' => 'issues#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
